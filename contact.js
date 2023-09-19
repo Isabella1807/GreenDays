fe0bc3a572c8400afa745f5f05e5143a2f9d752d
Isabella1807 <megaoreomuffin@hotmail.com>
2023-09-19 09:02:57 +0200

    Contact JS & Css

---
+++ contact.js
@@ -0,0 +1,37 @@
+const employeeContainer = document.getElementById("contactImgContainer");
+
+const employeeList = [
+    {
+        img: "pic.jpg",
+        titel: "Formand",
+        navn: "Claus Stougaard",
+        mail: "c_stougaard@hotmail.com",
+        number: "+45 93 92 12 66"
+    },
+    {
+        img: "pic.jpg",
+        titel: "Kasserer",
+        navn: "Jørgen Nielsen",
+        mail: "j.n.indramning@stofanet.dk",
+        number: "+45 22 48 47 57"
+    },
+];
+
+const addEmployeeToDOM = (name, title, image_url, mail, number) => {
+    employeeContainer.innerHTML += `
+    <div class="imgContainerItems">
+        <img src="${image_url}" alt="Billede af medarbejder ${name}">
+        <div class="imgContainerText">
+            <p>${name}</p>
+            <p>${title}</p>
+            <p>${mail}</p>
+            <p>${number}</p>
+        </div>
+    </div>
+    `;
+}
+
+for (let i = 0; i < employeeList.length; i++){
+    const employee = employeeList[i];
+    addEmployeeToDOM(employee.img, employee.titel, employee.navn, employee.mail, employee.number);
+}
